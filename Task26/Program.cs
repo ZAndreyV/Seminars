a96f9a506c9531ea317a1aff8563e57cddd8c55e
ZAndreyV <119517277+ZAndreyV@users.noreply.github.com>
2023-01-16 21:41:36 +0700

    Added Task24,26,28,30

---
+++ Task26/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 26: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.WriteLine("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int amountOfNumbers = AmountOfNumbers(number);
+Console.WriteLine($"{amountOfNumbers}");
+
+
+int AmountOfNumbers(int num)
+{
+    int result = 0;
+    while (num != 0)
+    {
+        num = num / 10;
+        result = result + 1;
+
+    }
+    return result;
+}
+
---
+++ Task28/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 28: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+
+Console.WriteLine("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = Factorial(number);
+Console.WriteLine($"{result}");
+//if (number <=)
+
+int Factorial(int num)
+{
+    int result = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        result *= i;
+    }
+    return result;
+}    
